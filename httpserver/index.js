c1c0e8c2fe278f5c2433e3619c6e48ee2f690170
Divyarajsinh <divyarajsinh.rana@archesoftronix.com>
2022-05-20 11:11:51 +0530

    node js routing http request

--- httpserver/index.js
+++ httpserver/index.js
@@ -1,9 +1,36 @@
 const http = require('http');
 
-const server = http.createServer((req,res)=>{
-    res.end('redirected to server');
+const server = http.createServer((req, res) => {
+    let uri = req.url;
+    switch (uri) {
+        case "/":
+            {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end('<h1>home page</h1>')
+            };
+            break;
+        case "/about":
+            {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end('<h1>about page</h1>');
+            }
+            break;
+        case "/contact":
+            {
+                res.writeHead(200, { "Content-type": "text/html" });
+                res.end('<h1>contact page</h1>');
+            }
+            break;
+        default:
+            {
+                res.writeHead(404, { "Content-type": "text/html" });
+                res.end("<h1>error 404!! page not found</h1>");
+            }
+            break;
+    }
+
 })
 
-server.listen(8080,'127.0.0.1',()=>{
+server.listen(8080, '127.0.0.1', () => {
     console.log("server run on port 8080");
 })

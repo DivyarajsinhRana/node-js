0907da52ca7fe7ae5434ae3bdf2b1bb296cc9d37
Divyarajsinh <divyarajsinh.rana@archesoftronix.com>
2022-05-23 11:55:28 +0530

    readable stream pipe method

--- stream-module/index.js
+++ stream-module/index.js
@@ -11,25 +11,31 @@ server.on('request', (req, res) => {
     //     if(error) return console.log(error);
     //     res.end(data);
     // })
-    // 2nd Way
-    // Reading fromaStream
-    // Createareadable stream
-    // Handle stream events data,end,and error
+
+    // // 2nd Way
+    // // Reading fromaStream
+    // // Createareadable stream
+    // // Handle stream events data,end,and error
+
+    // const rstream = fs.createReadStream('index.txt'); // created readable stream to read data from index.txt file
+    // // data event definition
+    // rstream.on('data', (chunkdata) => {
+    //     res.write(chunkdata)
+    // });
+    // // end event defintion
+    // rstream.on('end', () => {
+    //     res.end();
+    // });
+    // // error event defintion
+    // rstream.on('error', (error) => {
+    //     console.log("error...???", error);
+    //     res.end(error)
+    // });
+
+    // 3rd way using pipe
 
     const rstream = fs.createReadStream('index.txt'); // created readable stream to read data from index.txt file
-    // data event definition
-    rstream.on('data', (chunkdata) => {
-        res.write(chunkdata)
-    });
-    // end event defintion
-    rstream.on('end', () => {
-        res.end();
-    });
-    // error event defintion
-    rstream.on('error', (error) => {
-        console.log("error...???", error);
-        res.end(error)
-    });
+    rstream.pipe(res);
 });
 
 server.listen(8000, '127.0.0.1', () => { console.log("server run on port 8000") });

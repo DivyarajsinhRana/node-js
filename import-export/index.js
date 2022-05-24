0be507556ac6299b7fbbeafd7e5710ddc5963233
Divyarajsinh <divyarajsinh.rana@archesoftronix.com>
2022-05-24 11:47:04 +0530

    files delete

--- import-export/index.js
+++
@@ -1,6 +0,0 @@
-const  {add,name,sub} = require('./operator') //object destructering
-
-console.log(add(5,4));
-console.log(name);
-console.log(sub(6,5));
-
--- pathmodule/path.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-console.log(path.dirname("D:/Node/node-js/pathmodule/path.js"));
-console.log(path.extname("D:/Node/node-js/pathmodule/path.js"));
-console.log(path.basename("D:/Node/node-js/pathmodule/path.js"));
-const myPath = path.parse("D:/Node/node-js/pathmodule/path.js");
-console.log(myPath);
-console.log(myPath.base);

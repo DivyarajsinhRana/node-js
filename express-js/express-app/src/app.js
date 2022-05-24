fedb9119a4ccb5d94eb49e4948c6d604934fa496
Divyarajsinh <divyarajsinh.rana@archesoftronix.com>
2022-05-24 17:40:17 +0530

    rename views folder into template

--- express-js/express-app/src/app.js
+++ express-js/express-app/src/app.js
@@ -7,25 +7,26 @@ const port = 3000;
 // app.use(express.static(staticPath));      // built in middleware
 
 // server dynamic html using template engines
+const templatePath = path.join(__dirname,'../src/template')
+app.set('view engine', 'hbs');
+app.set('views',templatePath)      // rename views folder to template  
 
-app.set('view engine','hbs');
-
-app.get('/',(req,res)=>{
-    res.render('index',{channel:'divyaraj'});  // hbs file(index.hbs inside views directory)
+app.get('/', (req, res) => {
+    res.render('index', { channel: 'divyaraj' });  // hbs file(index.hbs inside views directory)
 });
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.send('<h1>welcome to home page</h1>');  // html
 });
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.status(200).send('welcome to about page')  //text
-}); 
-app.get('/contact',(req,res)=>{
+});
+app.get('/contact', (req, res) => {
     res.send('welcome to contact page')
 });
-app.get('/user',(req,res)=>{
-    res.send({"username":"Ram","password":"123"})           //json
+app.get('/user', (req, res) => {
+    res.send({ "username": "Ram", "password": "123" })           //json
 });
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`server run on port ${port}`)
 });

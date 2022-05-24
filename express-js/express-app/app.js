6cb48fdebd6fa9f5184fc518f581fd97977648a4
Divyarajsinh <divyarajsinh.rana@archesoftronix.com>
2022-05-24 14:14:25 +0530

    serve HTML and JSON as response

--- express-js/express-app/app.js
+++ express-js/express-app/app.js
@@ -4,14 +4,17 @@ const app = express();
 const port = 3000;
 
 app.get('/',(req,res)=>{
-    res.send('welcome to home page');
+    res.send('<h1>welcome to home page</h1>');  // html
 });
 
 app.get('/about',(req,res)=>{
-    res.status(200).send('welcome to about page')
-});
+    res.status(200).send('welcome to about page')  //text
+}); 
 app.get('/contact',(req,res)=>{
     res.send('welcome to contact page')
+});
+app.get('/user',(req,res)=>{
+    res.send({"username":"Ram","password":"123"})           //json
 });
 
 app.listen(port,()=>{

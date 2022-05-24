815f4f7db621f5dae55c83accd68ccee611ad62d
Divyarajsinh <divyarajsinh.rana@archesoftronix.com>
2022-05-24 12:40:11 +0530

    routing in express js

--- express-js/express-app/app.js
+++ express-js/express-app/app.js
@@ -4,7 +4,14 @@ const app = express();
 
 app.get('/',(req,res)=>{
     res.send('welcome to home page');
-})
+});
+
+app.get('/about',(req,res)=>{
+    res.status(200).send('welcome to about page')
+});
+app.get('/contact',(req,res)=>{
+    res.send('welcome to contact page')
+});
 
 app.listen(8000,()=>{
     console.log("server run on port 8000")
